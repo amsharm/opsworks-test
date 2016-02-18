640a4eb983ebf0edb2be4a89168c66d023b229db
Sharma <aman.f.sharma@accenture.com>
2016-02-18 11:24:30 -0600

    Second commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,8 @@
-Chef::Log.info("***Creating a data directory.***")
+Chef::Log.info("***Starting an instance***")
 
-directory 'C:\data' do
-  rights :full_control, 'WORKGROUP\aman.f.sharma'
-  inherits false
-  action :create
-end
+opsworks = Aws::OpsWorks::Client.new()
+opsworks.CreateInstanceRequest({
+  stack_id: "224b49c1-3fc2-4e4d-8371-963c7a8c3ac5"
+  layer_id: "79202cda-4802-47c0-8619-5444f29f2782"
+  instance_type: "t2.micro"
+})

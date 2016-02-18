24bfe3a66c26f0a405c00765ec59625b9fe06137
Sharma <aman.f.sharma@accenture.com>
2016-02-18 14:34:37 -0600

    Third commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,15 @@
 Chef::Log.info("***Starting an instance***")
 
-opsworks = Aws::OpsWorks::Client.new()
-opsworks.CreateInstanceRequest({
-  stack_id: "224b49c1-3fc2-4e4d-8371-963c7a8c3ac5"
-  layer_id: "79202cda-4802-47c0-8619-5444f29f2782"
-  instance_type: "t2.micro"
-})
+require 'chef/provisioning'
+require 'chef/provisioning/driver_init/aws'
+
+with_driver 'aws::us-east-1'
+
+aws_vpc 'blah' do
+  cidr_block '10.0.0.0/16'
+end
+
+machine 'mario' do
+  tag 'itsa_me'
+  converge true
+end
